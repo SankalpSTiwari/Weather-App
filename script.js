6c69a184463d42de0560ffd0c3f2e575724054c7
Sankalp Tiwari <41205888+SankalpSTiwari@users.noreply.github.com>
2023-11-04 21:49:42 -0700

    Added frontend and api calling functionality

---
+++ script.js
@@ -0,0 +1,23 @@
+const button = document.getElementById("search-button");
+const input = document.getElementById("city-input");
+
+const cityName = document.getElementById("city-name");
+const cityTime = document.getElementById("city-time");
+const cityTemp = document.getElementById("city-temp");
+async function getData(cityName){
+    const promise = fetch(
+        `http://api.weatherapi.com/v1/current.json?key=95cb1a5b390847a89b842712230511&q=${cityName}&aqi=yes`
+    )
+    return (await promise).json()
+}
+
+button.addEventListener("click", async () => {
+    const value = input.value;
+    const result = await getData(value);
+    cityName.innerText = `${result.location.name}, ${result.location.region} - ${result.location.country}`
+    cityTime.innerText = `${result.location.localtime}`
+    cityTemp.innerText = `${result.current.temp_c}`
+    console.log(result);
+})
+
+// http://api.weatherapi.com/v1/current.json?key=95cb1a5b390847a89b842712230511&q=London&aqi=yes
